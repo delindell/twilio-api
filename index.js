24eea172a6052fd444c03ada715208d8ed3ae59e
delindell <daviselindell@gmail.com>
2020-07-01 18:28:48 -0500

    recieved message

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config()
 
-app.post('/sms', (req, res) => {
-  console.log('sms url works');
+const jsonParser = bodyParser.json();
+
+app.post('/sms', jsonParser, (req, res) => {
+  const accountSid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN;
+  const fromNumber = process.env.PHONE_NUM;
+  const toNumber = req.body.sendNumber;
+  const message = req.body.message;
+
+  const client = require('twilio')(accountSid, authToken);
+  
+  client.messages
+    .create({
+       body: message,
+       from: fromNumber,
+       to: toNumber,
+     })
+    .then(message => {
+      console.log(message.sid);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err)
+    res.sendStatus(500)});
 });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
